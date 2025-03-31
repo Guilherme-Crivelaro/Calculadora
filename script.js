f3f85adbb95945962d63936b2bb6bccdd7793aa5
Guilherme-Crivelaro <crivelarogui@gmail.com>
2025-03-31 20:41:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function expressao(parametro){
+
+   // document.getElementById('display-calculadora').value +=parametro;
+
+    let valor = document.getElementById('display-calculadora').value;
+    valor = valor+parametro;
+    document.getElementById('display-calculadora').value=valor;
+
+}
+
+function calcular(){
+
+    try{
+        let resultado=eval(document.getElementById('display-calculadora').value);
+        document.getElementById('display-calculadora').value=resultado;
+    }catch(error){
+        document.getElementById('display-calculadora').value= "Expressão incorreta"
+    }
+
+}
+
+function zerar(){
+    document.getElementById('display-calculadora').value=null;
+}
